c5ff95f1eb65b6c9d07aa25a580313b4539396e8
muscaiu <muscaiu@yahoo.com>
2016-10-14 17:09:13 +0300

    #2

---
+++ js/controllers/MainController.js
@@ -0,0 +1,8 @@
+app.controller('MainController',['$scope', function($scope){
+  $scope.title = 'w00f';
+  $scope.promo = 'tits';
+  $scope.product = {
+    name: 'The Book of Trees',
+    price: 19
+  };
+}]);
