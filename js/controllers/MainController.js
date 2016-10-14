ee2104cceeb4df61cd43a68897bc0c466dc4e5e3
muscaiu <muscaiu@yahoo.com>
2016-10-14 17:25:13 +0300

    Add ionic app

--- js/controllers/MainController.js
+++
@@ -1,8 +0,0 @@
-app.controller('MainController',['$scope', function($scope){
-  $scope.title = 'w00f';
-  $scope.promo = 'tits';
-  $scope.product = {
-    name: 'The Book of Trees',
-    price: 19
-  };
-}]);
